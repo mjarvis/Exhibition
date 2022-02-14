62db37e67974ae2959778b657e3015faf5492b5b
Malcolm Jarvis <malcolm@metalab.co>
2022-02-14 10:33:07 -0800

    Add simple debug view
    Resolves #1
    
    Adds a simple debug view presented as a sheet over top of a given exhibit.
    
    This debug view currently provides a picker for changing the preferred color scheme, and an immutable list of the current exhibits parameters.

---
+++ Sources/Exhibition/DebugView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+/// A presented view used to modify accessibility and exhibit parameters.
+struct DebugView: View {
+    @ObservedObject var parameters: Exhibit.Parameters
+    
+    @Binding var preferredColorScheme: ColorScheme
+    
+    @Environment(\.dismiss) var dismiss
+    
+    init(parameters: Exhibit.Parameters, preferredColorScheme: Binding<ColorScheme>) {
+        self.parameters = parameters
+        _preferredColorScheme = preferredColorScheme
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section("Accessibility") {
+                    Picker("Color Scheme", selection: $preferredColorScheme) {
+                        Text("Light").tag(ColorScheme.light)
+                        Text("Dark").tag(ColorScheme.dark)
+                    }
+                }
+                
+                Section("Parameters") {
+                    ForEach(Array(parameters.values.keys), id: \.self) { key in
+                        Text("\(key)")
+                    }
+                }
+            }
+            .navigationTitle("Debug")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+        .preferredColorScheme(preferredColorScheme)
+    }
+}
--- Sources/Exhibition/Exhibition.swift
+++ Sources/Exhibition/Exhibition.swift
@@ -5,6 +5,10 @@ public struct Exhibition: View {
     let exhibits: [Exhibit]
 
     @State var displayed: AnyHashable?
+    @State var debugViewPresented: Bool = false
+    
+    // MARK: Accessibility
+    @State var preferredColorScheme: ColorScheme = .light
 
     func displayBinding(for id: AnyHashable) -> Binding<Bool> {
         Binding(
@@ -26,11 +30,31 @@ public struct Exhibition: View {
     public var body: some View {
         NavigationView {
             List(exhibits) { exhibit in
-                NavigationLink(exhibit.id, destination: exhibit)
+                NavigationLink(exhibit.id, destination: debuggable(exhibit))
             }
             .navigationTitle("Exhibit")
             .navigationBarTitleDisplayMode(.inline)
         }
+        .preferredColorScheme(preferredColorScheme)
+    }
+    
+    private func debuggable(_ exhibit: Exhibit) -> some View {
+        exhibit
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        debugViewPresented = true
+                    } label: {
+                        Image(systemName: "gear")
+                    }
+                }
+            }
+            .sheet(isPresented: $debugViewPresented) {
+                DebugView(
+                    parameters: exhibit.parameters,
+                    preferredColorScheme: $preferredColorScheme
+                )
+            }
     }
 }
 
